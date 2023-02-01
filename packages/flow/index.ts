e778d582cb54806159df84b438b071f55721f96d
Shelton Louis <louisshelton0@gmail.com>
2023-01-31 20:21:06 -0500

    Commiting all

--- packages/flow/index.ts
+++ packages/flow/index.ts
@@ -1,6 +1,6 @@
-export { default as For } from './src/For.astro';
-export { default as Case } from './src/Case.astro';
-export { default as When } from './src/When.astro';
-export { default as Switch } from './src/Switch.astro';
+export { default as For } from './lib/For.astro';
+export { default as Case } from './lib/Case.astro';
+export { default as When } from './lib/When.astro';
+export { default as Switch } from './lib/Switch.astro';
 
-export { iterate } from './src/iterate';
+export { iterate } from './lib/iterate';
